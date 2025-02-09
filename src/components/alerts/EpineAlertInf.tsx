ed38a8e788dc2a601065d51cc14a51f2f3df7cfc
Omar-Ztr <omarzouiter97@gmail.com>
2025-02-09 23:31:40 +0100

    Alerts

---
+++ src/components/alerts/EpineAlertInf.tsx
@@ -0,0 +1,34 @@
+import { ModelProps } from "../MenuBar";
+
+function EpineAlertInf({ closeModal }: ModelProps) {
+  return (
+    <div className="add-patient-form-modal" style={{ alignItems: "center" }}>
+      <div className="add-patient-form">
+        <h2 className="add-new-patient-form-title">Epinephrine Dose</h2>
+
+        <div className="form-column">
+          <div className="form-group">
+            <div>
+              {" "}
+              Click "Proceed" to record additional dose of epinephrine.{" "}
+            </div>
+            <div className="button-container">
+              <button
+                type="button"
+                className="cancel-button"
+                onClick={closeModal}
+              >
+                Cancel
+              </button>
+              <button type="button" className="submit-button">
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EpineAlertInf;
